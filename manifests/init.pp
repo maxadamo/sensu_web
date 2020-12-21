21818089bb743ccf5eb1880df2ce7438556a589e
Massimiliano Adamo <maxadamo@gmail.com>
2020-12-21 16:38:45 +0100

    move node modules elsewhere

--- manifests/init.pp
+++ manifests/init.pp
@@ -54,13 +54,20 @@ class sensu_web (
 
   $api_url = $sensu::api_url
 
-  file { 'sensu-web-dir':
+  file { [$install_dir, "${install_dir}/yarn"]:
     ensure => 'directory',
     path   => $install_dir,
     owner  => $service_user,
     group  => $service_group,
     mode   => '0755',
     before => Vcsrepo['sensu-web'],
+  }
+
+  # this is temporary (to clean some space)
+  -> file { "${install_dir}/node_modules":
+    ensure  => absent,
+    recurse => true,
+    force   => true;
   }
 
   vcsrepo { 'sensu-web':
@@ -75,16 +82,16 @@ class sensu_web (
 
   exec { 'sensu-web-touch-install':
     path        => '/usr/bin:/bin',
-    command     => "touch ${install_dir}/.install",
+    command     => "touch ${install_dir}/yarn/.install",
     refreshonly => true,
     user        => $service_user,
     before      => Exec['sensu-web-install'],
   }
   exec { 'sensu-web-install':
     path    => '/usr/bin:/bin:/usr/sbin:/sbin',
-    command => "yarn install && rm -f ${install_dir}/.install",
+    command => "yarn install --modules-folder ${install_dir}/yarn/node_modules && rm -f ${install_dir}/yarn/.install",
     cwd     => $install_dir,
-    onlyif  => "test -f ${install_dir}/.install",
+    onlyif  => "test -f ${install_dir}/yarn/.install",
     timeout => 0,
     user    => $service_user,
     require => Package['yarn'],
